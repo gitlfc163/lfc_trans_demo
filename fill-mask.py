d43e866612e20e1af03c42b09f68b5b813ba6eec
longfc <longfuchu@163.com>
2023-06-13 18:10:00 +0800

    init

--- fill-mask.py
+++ fill-mask.py
@@ -1,12 +1,20 @@
 
-## 文本填充示例
+# 文本填充示例
 
 from transformers import pipeline
 
+# 英文示例
 # 初始化文本填充器
 unmasker = pipeline("fill-mask") 
-
 # 输入文本
 results = unmasker("This course will teach you all about <mask> models.", top_k=2)
-
 print(results)
+
+
+# 中文示例
+# 初始化文本填充器
+generatorCH = pipeline('fill-mask','bert-base-chinese')
+# 生成文本
+resultsCH =generatorCH("生活的真谛是[MASK]。")
+# 输出结果
+print(resultsCH)
--- text-generation.py
+++ text-generation.py
@@ -2,10 +2,20 @@
 
 from transformers import pipeline
 
+# 英文示例
 # 初始化文本生成器
 generator = pipeline("text-generation")
-
 # 生成文本
-results =generator("In this course, we will teach you how to")
+resultsEN =generator("In this course, we will teach you how to")
+print(resultsEN)
 
-print(results)
+
+# 文本生成中文示例
+# pip3 install jieba
+from transformers import TextGenerationPipeline, AutoTokenizer, AutoModelWithLMHead
+
+tokenizer = AutoTokenizer.from_pretrained("TsinghuaAI/CPM-Generate")
+model = AutoModelWithLMHead.from_pretrained("TsinghuaAI/CPM-Generate")
+
+text_generator = TextGenerationPipeline(model, tokenizer)
+text_generator('清华大学', max_length=50, do_sample=True, top_p=0.9)
